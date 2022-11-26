3d7ba9caed2ce4011e9a9c0a0109fdd08fa42211
quarkape <quarkape@qq.com>
2022-11-27 02:06:00 +0800

    big update to V2.0.0

--- js/content.js
+++ js/content.js
@@ -1,7 +1,9 @@
-console.log(`%c${'欢迎使用Free Logo插件.\n项目地址:https://github.com/quarkape/free_logo.git.\n还请给个star,欢迎fork~'}`, 'font-size: 20px;color: #9c26b0;font-weight: bold;')
+console.log(`%c${'欢迎使用Free Logo插件.\n项目地址:https://github.com/quarkape/Free_Logo.git.\n还请给个star,欢迎fork~'}`, 'font-size: 20px;color: #9c26b0;font-weight: bold;')
 
 chrome.runtime.onMessage.addListener((req, sender, resp) => {
+  console.log(req)
   console.log('processing...')
+  // is deleting login cover
   if (req.del) {
     const logincover = document.getElementsByClassName("el-scrollbar__view")[0].children[7];
     if (!logincover.className) {
@@ -13,30 +15,80 @@ chrome.runtime.onMessage.addListener((req, sender, resp) => {
     }
     return;
   }
-  let widthset = req.widthset === '' ? 620 : parseInt(req.widthset);
+  // is bzk
+  const bxz = {
+    'min': 34,
+    'max': 6800,
+    'default': 680,
+    'w': 34,
+    'h': 25
+  }
+  const bzk = {
+    'min': 31,
+    'max': 6200,
+    'default': 620,
+    'w': 4,
+    'h': 3
+  }
+
+  let uname = req.uname;
+  let widthset = req.widthset === '' ? (uname === 'bxz' ? bxz.default : bzk.default) : parseInt(req.widthset);
   let keepbg = req.keepbg;
-  if (widthset < 62 || widthset > 6200) {
-    resp('请确保值介于62~6200');
+  if (uname === 'bxz') {
+    if (widthset < 34 || widthset > 6800) {
+      resp('宽度应该介于34~6800');
+      return;
+    }
+  } else if (uname === 'bzk') {
+    if (widthset < 62 || widthset > 6200) {
+      resp('宽度应该介于31~6200');
+      return;
+    }
+  } else {
+    resp('意外的错误，烦请到github上面提issue~')
     return;
   }
-  const svg_part = document.getElementsByTagName("logomaker-logo-editor")[0].shadowRoot.lastChild.childNodes[0].childNodes[0].children[0];
+  
+  let svg_part = null;
+  if (uname === 'bxz') {
+    svg_part = document.getElementsByClassName("card__upper")[0].parentNode.childNodes[0].children[1];
+  } else {
+    svg_part = document.getElementsByTagName("logomaker-logo-editor")[0].shadowRoot.lastChild.childNodes[0].childNodes[0].children[0];
+  }
+  console.log(svg_part)
   const svg_part_copy = svg_part.cloneNode(true);
   svg_part_copy.setAttribute("width", widthset);
-  svg_part_copy.setAttribute("height", parseInt(widthset * 3 / 4));
-  const bg = svg_part_copy.children[1];
-  if (bg.nodeName === 'g' || bg.id === 'background') {
-    if (!keepbg) {
-      svg_part_copy.removeChild(bg);
+  svg_part_copy.setAttribute("height", parseInt(widthset * (uname === 'bxz' ? bxz.w : bzk.w) / (uname === 'bxz' ? bxz.h : bzk.h)));
+  let bg = null;
+  if (uname === 'bxz') {
+    bg = svg_part_copy.children[0];
+    if (bg.nodeName === 'rect' || bg.className === 'background') {
+      if (!keepbg) {
+        svg_part_copy.removeChild(bg);
+      }
+    }
+    const wm = svg_part_copy.children[1];
+    if (wm.nodeName === 'rect' || wm.classname === 'watermarklayer') {
+      svg_part_copy.removeChild(wm);
+    }
+  } else {
+    bg = svg_part_copy.children[1];
+    if (bg.nodeName === 'g' || bg.id === 'background') {
+      if (!keepbg) {
+        svg_part_copy.removeChild(bg);
+      }
     }
   }
-  dl(svg_part_copy.outerHTML);
+
+  // final
+  dl(svg_part_copy.outerHTML, uname);
   console.log('succeed!')
   resp('操作成功')
 })
 
-function dl(content) {
+function dl(content, uname) {
   const el = document.createElement("a");
-  el.setAttribute('download', 'free_logo.svg');
+  el.setAttribute('download', uname === 'bxz' ? 'bxz_logo.svg' : 'bzk_logo.svg');
   const blob = new Blob([content])
   el.setAttribute('href', URL.createObjectURL(blob));
   el.style.display = 'none';
--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,18 @@
 document.getElementById("execute").addEventListener("click", () => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     const url = tabs[0].url;
-    if (url.indexOf('https://www.logomaker.com.cn/guide#/generate') === -1) {
-      document.getElementById("ans").innerText = "当前页面并不适合使用该插件，请点击上方使用教程查看本插件适用网站与页面";
+    let opt = {};
+    if (url.indexOf('https://www.logosc.cn/edit') === 0) {
+      opt.uname = 'bxz';
+    } else if (url.indexOf('https://www.logomaker.com.cn/guide#/generate') === 0) {
+      opt.uname = 'bzk';
+    } else {
+      document.getElementById("ans").innerText = "当前网站或页面不适合使用该插件。如果判断有误，烦请到github上面提issue~";
       return;
     }
-    let opt = {
-      'del': false,
-      'widthset': document.getElementById("widthset").value,
-      'keepbg': document.getElementById('keepbg').checked
-    }
+    opt.del = false;
+    opt.widthset = document.getElementById("widthset").value;
+    opt.keepbg = document.getElementById('keepbg').checked;
     chrome.tabs.sendMessage(tabs[0].id, opt, (resp) => {
         document.getElementById("ans").innerText = resp;
     })
@@ -24,7 +27,7 @@ document.getElementById("deletecover").addEventListener("click", () => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     const url = tabs[0].url;
     if (url.indexOf('https://www.logomaker.com.cn/guide#/generate') === -1) {
-      document.getElementById("ans").innerText = "当前页面并不适合使用该插件，请点击上方使用教程查看本插件适用网站与页面";
+      document.getElementById("ans").innerText = "当前页面并不存在登录框。如果判断有误，烦请到github上面提issue~";
       return;
     }
     let opt = {
